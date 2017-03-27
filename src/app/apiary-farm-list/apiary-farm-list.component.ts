ea5930a0e6e31535243bf70146b5c58fca38e729
JPZ <jeanpaulzune@gmail.com>
2017-03-27 00:14:14 -0400

    Fixed some issues within apiary-farm-list component

--- src/app/apiary-farm-list/apiary-farm-list.component.ts
+++ src/app/apiary-farm-list/apiary-farm-list.component.ts
@@ -2,12 +2,13 @@ import { Component, OnInit } from '@angular/core';
 import { ApiaryFarmService } from '../apiary-farm.service'
 
 @Component({
-  selector: 'app-bee-farm-list',
-  templateUrl: './bee-farm-list.component.html',
-  styleUrls: ['./bee-farm-list.component.scss'],
+  selector: 'app-apiary-farm-list',
+  templateUrl: './apiary-farm-list.component.html',
+  styleUrls: ['./apiary-farm-list.component.scss'],
   providers: [ApiaryFarmService]
 })
-export class BeeFarmListComponent implements OnInit {
+
+export class ApiaryFarmListComponent implements OnInit {
   apiaryFarms: Array<Object> = [];
   errorMessage: string = '';
 
